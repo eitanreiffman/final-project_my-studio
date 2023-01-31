282dcd92880815902b7ed132867c128cebdfdfce
Eitan Reiffman <eitanreiffman@gmail.com>
2023-01-31 00:31:40 -0500

    Initialize Server and Router

---
+++ routes/users.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send("User List")
+})
+
+router.get('/new', (req, res) => {
+    res.send("User New Form")
+})
+
+router.post('/', (req, res) => {
+    res.send('Create User')
+})
+
+router.route('/:id')
+.get((req, res) => {
+    console.log(req.user)
+    res.send(`Get User With ID ${req.params.id}`)
+})
+.put((req, res) => {
+    res.send(`Get User With ID ${req.params.id}`)
+})
+.delete((req, res) => {
+    res.send(`Get User With ID ${req.params.id}`)
+})
+
+const users = [{ name: 'Jonah' }, { name: 'Shira' }, { name: 'Eitan' }]
+
+router.param('id', (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    console.log("Here")
+    res.render('index')
+})
+
+const userRouter = require('./routes/users')
+app.use('/users', userRouter)
+
+app.listen(3000)
