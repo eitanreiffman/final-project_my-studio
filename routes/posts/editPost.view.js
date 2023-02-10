2a4372394e093fb9626bcfb8127e38efad052b4e
Eitan Reiffman <eitanreiffman@gmail.com>
2023-02-10 00:19:30 -0500

    Added values for Edit Post form

--- routes/posts/editPost.view.js
+++ routes/posts/editPost.view.js
@@ -1,4 +1,33 @@
+const axios = require('axios')
 
 module.exports = async (req, res) => {
-    res.render('posts/editPost', { userLoggedIn: req.verifiedUser })
+    id = req.params.id
+    const query = `
+        query ($id: ID!){
+            post(id: $id){
+            id
+            title
+            description
+            songFile
+            user{
+                username
+            }
+        }
+    }
+`
+    const { data } = await axios.post(process.env.GRAPHQL_ENDPOINT,
+        {
+            query: query,
+            variables: { id }
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    )
+    const post = data.data.post
+
+
+    res.render('posts/editPost', { userLoggedIn: req.verifiedUser, post: post })
 }
